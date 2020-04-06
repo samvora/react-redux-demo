e01d0000c95168d8ddbdbdcddccfa2604496328d
Smit Vora <smit.vora1@gmail.com>
2020-04-06 13:38:48 +0530

    search and filter changes

--- src/containters/Home/Home.js
+++ src/containters/Home/Home.js
@@ -1,22 +1,87 @@
-import React, { useState, useReducer } from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
 import Layout from '../../layout';
+import axios from 'axios';
 
 const Home = () => {
 
-console.log('Home')
-    const [search, setSearch] = useState('');
+    console.log('Home')
+    const [filter, setFilter] = useState({
+        search: '',
+        currentPage: 1,
+        perPage: 5,
+        totalPages: 0,
+        filterTodos: [],
+    });
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        // it will be called only when component first time render
+        axios.get('https://jsonplaceholder.typicode.com/todos').then(res => {
+            console.log(res.data);
+            setTodos(res.data);
+        });
+    }, []);
+
+    useEffect(() => {
+        let updatedFilteredTodos = todos.filter(todo => {
+            if (filter.search != '') {
+                return todo.title.includes(filter.search);
+            }
+            else {
+                return true;
+            }
+        });
+        let totalPages = Math.ceil(updatedFilteredTodos.length / filter.perPage);
+        if (updatedFilteredTodos.length > 0) {
+            updatedFilteredTodos = updatedFilteredTodos.splice(filter.perPage * (filter.currentPage - 1), filter.perPage);
+        }
+
+        setFilter({ ...filter, filterTodos: updatedFilteredTodos, totalPages });
+    }, [filter.search, todos, filter.currentPage]);
+
 
     const handleSerach = (event) => {
         const newValue = event.target.value;
-        setSearch(newValue);
+        setFilter({ ...filter, search: newValue, currentPage: 1 });
+    };
+
+
+
+
+    const totalPagesArray = Array.from({ length: filter.totalPages }, (_, index) => index + 1);
+
+    const paginationComponent = totalPagesArray.map(pageNumber => {
+        return <div onClick={() => changePage(pageNumber)} key={[pageNumber]}>{pageNumber}</div>
+    })
+
+    const changePage = (nextPage) => {
+        setFilter({ ...filter, currentPage: nextPage });
     }
+
+
 
     return (
         <Layout>
             <div>
-                <input type="text" onChange={handleSerach} value={search} />
-                <Counter initialCount={1} />
+                <input type="text" onChange={handleSerach} value={filter.search} />
+                {/* <Counter initialCount={1} /> */}
             </div>
+            <table>
+                <thead><tr><th>{filter.currentPage}</th></tr></thead>
+                <tbody>
+                    {filter.filterTodos.map((todo, index) => {
+                        return (
+                            <tr key={index}><td>{todo.title}</td></tr>
+                        );
+                    })}
+                    <tr>
+                        <td>
+                            {paginationComponent}
+                        </td>
+                    </tr>
+                </tbody>
+
+            </table>
 
         </Layout>
     );
